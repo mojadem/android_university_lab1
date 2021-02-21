b6b85aeeee720cc6875fa7f5544d2d5691786cbd
mojadem <dembiczakmatt@gmail.com>
2021-02-21 16:07:30 -0500

    Updated the devloper api key for nyt

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "4XCWaO59kxePnRK7WKPAYPsqpR2zCIfi";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
